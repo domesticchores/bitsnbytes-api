3c82fef694ae939fc65f08c0e16855963ce8d9bc
IsaacIngram <me@isaacingram.dev>
2026-07-18 10:39:21 -0700

    Fix bug

--- api/__init__.py
+++ api/__init__.py
@@ -316,7 +316,7 @@ def add_nfc_data():
 
         # Get user with this phone from DB if phone is valid and user exists
         if data['phone'] is not None and data['phone'] != '':
-            existing_phone_user = db.session.query(User).filter(User.phone == data['phone]']).first()
+            existing_phone_user = db.session.query(User).filter(User.phone == data['phone']).first()
         else:
             existing_phone_user = None
 
